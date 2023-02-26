32fb99f8a222811e99b41c96ba9f96caaecd0ba0
Omkar <10015433@sbstudents.org>
2023-02-26 14:29:33 -0500

    scripts

--- times.js
+++ times.js
@@ -1,4 +1,15 @@
-function getPop(){
+/*function getPop(){
     document.write(5 + 6);
     document.write(livepopulartimes.get_populartimes_by_address("(Sonny Werblin Recreation Center) 656 Bartholomew Rd, Piscataway, NJ, United States")[current_popularity]);
-}
+}*/
+
+const spawner = require('child_process').spawn;
+
+const data_to_pass_in = 'Send this to python script';
+console.log('Data sent to python script:');
+
+const python_process = spawner('python', ['./times.py', data_to_pass_in]);
+
+python_process.stdout.on('data', (data) => {
+    console.log('Data recieved from python script:', data.toString());
+});
--- times.py
+++ times.py
@@ -1,7 +1,15 @@
 import populartimes
 import livepopulartimes
 import json
+import sys
 
-json_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJjb5XVwvHw4kRZ8YebnLtc0o")
+#json_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJjb5XVwvHw4kRZ8YebnLtc0o")
+#print(json_obj['current_popularity'])
 
-print(json_obj['current_popularity'])
+data_to_pass_back = 'Send this to node process.'
+
+input = sys.argv[1]
+output = data_to_pass_back
+print(output)
+
+sys.stdout.flush()
