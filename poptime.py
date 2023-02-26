b2dcdb0dc54239fb1e88262bd560b6a816c49a5c
Omkar <omkad03@gmail.com>
2023-02-26 16:45:22 -0500

    python flask server

---
+++ poptime.py
@@ -0,0 +1,32 @@
+import populartimes
+import livepopulartimes
+import json
+import sys
+from flask import Flask, redirect, url_for, render_template
+from datetime import datetime
+
+day = datetime.now().weekday
+hour = datetime.now().hour
+
+app = Flask(__name__)
+
+werblin_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJjb5XVwvHw4kRZ8YebnLtc0o")
+werblin = werblin_obj['current_popularity']
+
+cag_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJHVMZ-f_Gw4kR5-ZZCMoenYk")
+#cag = cag_obj['populartimes'][day]["data"][hour]
+
+livi_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJvcb4esDHw4kRqKj17FN9pJA")
+#livi = [a['data'][hour] for a in livi_obj['populartimes']['data'] if a['name']==day]
+
+cd_obj = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDDifvQyJnQ38Rm3XpBi-NUxCx7nKtLbrs", "ChIJ0wwzrWvGw4kR8X5vv1n9Rxg")
+cd = cd_obj['current_popularity']
+
+@app.route("/")
+def home():
+    return render_template("capacity.html", werblin=werblin, cag=werblin-10, livi=werblin+5, cd=cd)
+
+if __name__ == "__main__":
+    app.run()
+
+sys.stdout.flush()
--- times.js
+++ times.js
@@ -8,7 +8,7 @@ const spawner = require('child_process').spawn;
 const data_to_pass_in = 'Send this to python script';
 console.log('Data sent to python script:');
 
-const python_process = spawner('python', ['./times.py', data_to_pass_in]);
+const python_process = spawner('python', ['./poptime.py', data_to_pass_in]);
 
 python_process.stdout.on('data', (data) => {
     console.log('Data recieved from python script:', data.toString());
