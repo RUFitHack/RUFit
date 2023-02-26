76405001bb5700f843ae29c30a3949d98c3e4726
Jojy paul <jojypaul@nbp-74-131.nbp.ruw.rutgers.edu>
2023-02-26 18:54:14 -0500

    idk

--- backend3.js
+++ backend3.js
@@ -15,97 +15,95 @@ var firebaseConfig = {};
   var CookBasketemailQueue = [];
   var CookVolleyemailQueue = [];
   
-  var WerbBasket;
-  var WerbVolley;
-  var Werbbadmin;
-  var LiviBasket;
-  var LiviVolley;
-  var CABasket;
-  var CAvolley;
-  var CookBasket;
-  var CookVolley;
 
- if(werbBasket){
-  database.ref('emails').once('value')
+  document.getElementById("werbBasket").addEventListener("click", myFunction1);
+  document.getElementById("werbVolley").addEventListener("click", myFunction2);
+  document.getElementById("CollegeBasket").addEventListener("click", myFunction3);
+  document.getElementById("CollegeVolley").addEventListener("click", myFunction4);
+  document.getElementById("LivingstonBasket").addEventListener("click", myFunction5);
+  document.getElementById("LivingstonVolley").addEventListener("click", myFunction6);
+  document.getElementById("Cook/Doug Basket").addEventListener("click", myFunction7);
+  document.getElementById("Cook/Doug Volley").addEventListener("click", myFunction8);
+
+  function myFunction1() {
+    database.ref('emails').once('value')
     .then(function(snapshot) {
       snapshot.forEach(function(childSnapshot) {
         var email = childSnapshot.val().email;
         WerbBasketemailQueue.push(email);
       });
-      console.log('Emails added to queue:', WerbBasketemailQueue);
+      alert('Emails added to queue:', WerbBasketemailQueue); 
     })
-}
-else if(WerbVolley){
-    database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          WerbVolleyemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', WerbVolleyemailQueue);
-      })
+ 
   }
-  else if(WerbBdmin){
+  function myFunction2() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          WerbBadminemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', WerbBadminemailQueue);
-      })
-  }
- else if(LiviBasket){
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        WerbVolleyemailQueue.push(email);
+      });
+     al('Emails added to queue:', WerbVolleyemailQueue);
+    })
+    }
+ 
+  
+  function myFunction3() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          LiviBasketemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', LiviBasketemailQueue);
-      })
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        CAbasketemailQueue.push(email);
+      });
+      console.log('Emails added to queue:', CAbasketemailQueue);
+    })
+ 
   }
-  else if(LiviVolley){
+  function myFunction4() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          LiviVolleyemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', LiviVolleyemailQueue);
-      })
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        CAvolleyemailQueue.push(email);
+      });
+      console.log('Emails added to queue:', CAvolleyemailQueue);
+    })
+ 
   }
-  else if(CABasket){
+  function myFunction5() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          CAbasketemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', CAbasketemailQueue);
-      })
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        LiviBasketemailQueue.push(email);
+      });
+      console.log('Emails added to queue:', LiviBasketemailQueue);
+    })
+ 
   }
-  else if(CAvolley){
+  function myFunction6() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          CAvolleyemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', CAvolleyemailQueue);
-      })
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        LiviVolleyemailQueue.push(email);
+      });
+      console.log('Emails added to queue:', LiviVolleyemailQueue);
+    })
+ 
   }
-  else if(CookBasket){
+  function myFunction7() {
     database.ref('emails').once('value')
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var email = childSnapshot.val().email;
-          CookBasketemailQueue.push(email);
-        });
-        console.log('Emails added to queue:', CookBasketemailQueue);
-      })
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var email = childSnapshot.val().email;
+        CookBasketemailQueue.push(email);
+      });
+      console.log('Emails added to queue:', CookBasketemailQueue);
+    })
+ 
   }
- else if(CookVolley){
+  function myFunction8() {
     database.ref('emails').once('value')
       .then(function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
@@ -114,6 +112,9 @@ else if(WerbVolley){
         });
         console.log('Emails added to queue:', CookVolleyemailQueue);
       })
-  }    
+ 
+  }
+
+ 
 
 
